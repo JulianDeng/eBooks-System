bf1288c76a7a8e08516f7a8507ce79c42768f575
JulianDeng <dzhongr@my.yorku.ca>
2017-03-26 06:07:11 -0400

    add signatures for orderProcessModel

--- src/model/OrderProcessModel.java
+++ src/model/OrderProcessModel.java
@@ -1,5 +1,26 @@
 package model;
 
-public class OrderProcessModel {
+import java.util.Map;
 
+public class OrderProcessModel {
+	
+	public OrderProcessModel(){
+		
+	}
+	
+	public boolean login(String user, String pswd){
+		return false;
+	}
+	
+	public boolean create(Map<String, String> data){
+		return false;
+		
+	}
+	
+	public boolean veryfy(String CCN){
+		//3rd time submit will fail
+		return false;
+	}
+	
+	
 }
