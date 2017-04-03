b3b2cd7bf06afd16777c9ee723713e8ca45a8343
JulianDeng <dzhongr@my.yorku.ca>
2017-04-03 07:35:24 -0400

    04032017 notable fix
    1. resolve rate button null protection issue.
    2. redraw UI
    3. reorganize logic in controller
    4. seperate category selector and search box (still need to redesign
    model function)
    5. sepearte Admin servlet from Home servlet
    6. refactor all wrong spelling "review " to "preview"
    7. add comment date

--- WebContent/res/Home.js
+++ WebContent/res/Home.js
@@ -1,3 +1,18 @@
+function validateRate(element){
+ 	var ok = false;
+ 	var checkboxs = element;
+ 	for(var i = 0, l = checkboxs.length; i < l; i++){
+ 		if(checkboxs[i].checked){
+ 			ok = true;
+ 			break;			
+ 		}
+ 	}
+ 	if(ok == false){
+ 		document.getElementById("msgRate").innerHTML = 'Please select at least one score!';
+ 	}
+ 	return ok;
+}
+
 function addBookToCartAjax(address, bookID){
 	var request = new XMLHttpRequest();
 	var data="?boodAddToCart="+bookID;
