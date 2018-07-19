aea0dee71260ae3504fccdc4f68b78972077234f
JulianDeng <dzhongr@my.yorku.ca>
2018-07-19 00:26:27 -0400

    Some changes
    add some REST services and minor UI fixes.

--- src/model/OrderWrapper.java
+++ src/model/OrderWrapper.java
@@ -4,10 +4,13 @@ import java.util.*;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 
 import bean.OrderBean;
 
 @XmlRootElement(name="orderList")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class OrderWrapper {
 	@XmlElement(name="orderItem")
 	private ArrayList<OrderBean> items;
--- src/web_service/OrderProcessService.java
+++ src/web_service/OrderProcessService.java
@@ -1,19 +1,28 @@
 package web_service;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 
+import bean.OrderBean;
 import model.OrderProcessModel;
 import model.OrderWrapper;
 
-@Path("OrderProcessService")
+@Path("/OrderProcessService")
 public class OrderProcessService {
 	
 	OrderProcessModel orderDao;
 	
 	@Context
 	private ServletContext context;
+	
+	@Context
+	private HttpServletRequest request; 
+	
+	@Context
+	private HttpServletResponse response;
 	
 	
 	public OrderProcessService() throws ClassNotFoundException{
@@ -27,7 +36,28 @@ public class OrderProcessService {
 	public String getOrdersByPartNumber(@PathParam("partNumber") String partNumber) throws Exception{
 		String result="Nothing for sale, please try again later";
 		OrderWrapper wrapper = orderDao.getOrder(partNumber);
-		String xml = orderDao.exportOrder(wrapper, context);
-		return xml;
+		result = orderDao.exportOrder(wrapper, context);
+		return result;
+	}
+	
+	@GET
+	@Path("/ordersXML/{partNumber}")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public OrderWrapper getOrderByPartNumberXML(@PathParam("partNumber") String partNumber) throws Exception{
+		OrderWrapper wrapper = orderDao.getOrder(partNumber);
+		return wrapper;
+	}
+	
+	@GET
+	@Path("/ordersHTML/{partNumber}")
+	@Produces("text/html")
+	public void getOrderByPartNumberHTML(@PathParam("partNumber") String partNumber) throws Exception{
+		OrderWrapper wrapper = orderDao.getOrder(partNumber);
+		OrderBean order = wrapper.getItems().get(0);
+		request.getSession().setAttribute("order", order);
+		
+		String target = "/RESTHTML.jspx";
+		request.getRequestDispatcher(target).forward(request, response);
+		return;
 	}
 }
